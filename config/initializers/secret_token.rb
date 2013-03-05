5abb5094d9b39f2321e745b3a027dcc3c0761738
coolashelvetica <fara.ashiru@gmail.com>
2013-03-05 15:42:20 -0500

    intialized rails app for Jiloak website using mysql and added bootstrap and compass sass, as well as simple form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jiloak::Application.config.secret_token = 'd81bf52d73acbe1ac4f65157bce024d38ba12a362fb5e478ed05e57e7130689f7a1029de905d0d74ce50b77b740c685b23c5d458f20d28adf4fc43c534100604'
